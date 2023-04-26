3a7d0b0fe57cd30448ef3d6ac618d0ba79f1fc27
Nicolas Lemus <nicolaswaldemarlemuscurbelo@gmail.com>
2023-04-25 23:51:53 -0300

    agregamos carrito externo

--- js/carrito.js
+++ js/carrito.js
@@ -1,69 +1,4 @@
-const calzados = [
-    {nombre: "Nike", precio: 4000 , tipo:"Deportivo"},
-    {nombre: "Adidas", precio: 3900, tipo:"Deportivo"},
-    {nombre: "Under", precio: 5000, tipo:"Deportivo"},
-    {nombre: "Puma", precio: 4900, tipo:"Deportivo"},
-    {nombre: "New Balance", precio: 5200, tipo:"Deportivo"},
 
-    {nombre: "Botas", precio: 5000, tipo:"Casual"},
-    {nombre: "Nauticas", precio: 4900, tipo:"Casual"},
-    {nombre: "Sneakers", precio: 6000, tipo:"Casual"},
-    {nombre: "Levi’s", precio: 5390, tipo:"Casual"},
-    {nombre: "Converse", precio: 5999,  tipo:"Casual"},
-    
-    {nombre: "Cuero", precio: 7000, tipo:"Formal"},
-    {nombre: "Zapatos sin cordones", precio: 5900, tipo:"Formal"},
-    {nombre: "Desert", precio: 6900, tipo:"Formal"},
-    {nombre: "Oxford", precio: 6199, tipo:"Formal"},
-    {nombre: "Gaziano & G", precio: 10000, tipo:"Formal"}
-];
-let nuevoProducto = new agregarProductos("Reebok", 4500, "Deportivo");
-//agregamos la variable a Calzados
-calzados.push(nuevoProducto);
-//sin stock
-calzados.splice(5,1);
-
-function agregarProductos(nombre,precio,tipo){
-    this.nombre=nombre;
-    this.precio=precio;
-    this.tipo=tipo;
-}
-
-//filtro de busqueda
-const buscador=document.querySelector("#buscar");
-buscador.addEventListener("keyup" , e => {
-    if(e.target.matches("#buscar")){
-        document.querySelectorAll(".card").forEach(calz =>{
-            calz.textContent.toLocaleLowerCase().includes(e.target.value)
-            ? calz.classList.remove("filtro")
-            :calz.classList.add("filtro");
-        })
-    }
-});
-
-//filtrado por precio
-const productos = document.querySelectorAll('.card');
-function filtrarPorPrecio(precioMaximo) {
-    for (let i = 0; i < productos.length; i++) {
-        const producto = productos[i];
-        const precio = parseInt(producto.querySelector('p').textContent.split('$')[1]);
-        if (precio > precioMaximo) {
-            producto.classList.add('filtro');
-        } else {
-            producto.classList.remove('filtro');
-        }
-    }
-}
-
-const inputPrecioMaximo = document.getElementById('precioMaximo');
-inputPrecioMaximo.addEventListener('input', function() {
-    const precioMaximo = parseInt(this.value);
-    filtrarPorPrecio(precioMaximo);
-});
-
-//talles
-
-const talles = [40, 41, 42, 43, 44];
 const productosTalles = document.querySelectorAll('.card');
 const carrito = document.querySelector('.listado');
 const contadorCarrito = document.querySelector('#valorCarrito');
@@ -101,7 +36,6 @@ function actualizarCarrito() {
     });
     contadorCarrito.textContent = cantidadProductos;
 }
-
 productosTalles.forEach((producto,) => {
     const nombreProducto = producto.querySelector('.card-title').textContent;
     const precioProducto = producto.querySelector('.card-text').textContent;
@@ -167,24 +101,37 @@ function actualizarPrecioTotal() {
     precioTotal.textContent  = `$${precioTotalCarrito.toFixed(2)}`;
 }
 
-//consumir esos datos en forma json.
-
-fetch("./data/datos.json")
-.then((resultado) => resultado.json())
-.then((data)=>{
-    console.log(data);
+carrito.addEventListener('click', (event) => {
+    if (event.target.classList.contains('eliminar-producto')) {
+    const index = event.target.dataset.index;
+    carritoProductos.splice(index, 1);
+    guardarProductosEnLocalStorage();
+    cantidadProductos--;
+    actualizarCarrito();
+    actualizarPrecioTotal();
+    }
+}); 
+const botonComprar = document.querySelector("#confirmarCompra");
+botonComprar.addEventListener("click", () => {
+    if (parseFloat(precioTotal.textContent.slice(1)) === 0) {
+        Swal.fire({
+            icon: 'error',
+            title: "Verificar!",
+            text: 'Tu Carrito esta vacio!',
+            timer: 3000,
+            })
+    } else {
+    Swal.fire({
+        icon: "success",
+        title: "Gracias por su compra",
+        showConfirmButton: false,
+        timer: 3000,
+    });
+    carritoProductos = [];
+    cantidadProductos = 0;
+    actualizarCarrito();
+    actualizarPrecioTotal();
+    localStorage.removeItem("carritoProductos");
+    localStorage.removeItem("cantidadProductos");
+    }
 });
-
-// aysnc await
-//transformar a una funcion asincrona
-
-async function fetchApi(){
-    const responsive = await fetch("./data/datos.json");
-    const data = await responsive.json();
-    console.log(data);
-}
-fetchApi();
-//forma mas moderna de usar asincronia y promasa.
-
-
-
